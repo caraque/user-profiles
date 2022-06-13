4f65dba5e7f93de6a3180759af675d96322cb7a4
Christian Araque <christaraque@hotmail.com>
2022-06-13 01:13:07 -0400

    feat(key): added key to user list

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <Grid container spacing={4} columns={{xs: 4, sm: 8, md: 12}} style={{marginTop: 10, marginBottom: 30}}>
             {
                 profiles.map((profile: any) =>
-                    <Grid item xs={4}>
+                    <Grid item xs={4} key={profile.email}>
                         <Profile key={profile.id.value} profile={profile}/>
                     </Grid>
                 )
